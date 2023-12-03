4d18875c234ebe5066e3b1d7afb15f58a6c6fee3
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-03 20:40:08 +0530

    callbacks

--- 0-1 cohort/Week 1/Class 1/main.js
+++ 0-1 cohort/Week 1/Class 1/main.js
@@ -80,7 +80,6 @@ for(let i = 0; i<allUsers.length; i++){
 }
 
 //Functions
-
 function sum(a, b){
 // do things with the input and return an output
   const sumValue = a + b;
@@ -89,5 +88,22 @@ function sum(a, b){
 const value = sum(1,2);
 const value2 = sum(1,10);
 console.log(value);
+
+
+// Callbacks - passing a whole function as an argument
+function sum(num1, num2, fnToCall) {
+  let result = num1 + num2;
+  //return result;
+  fnToCall(result);
+}
+function displayResult(data) {
+  console.log("Result of the sum is : " + data);
+}
+function displayResultPassive(data) {
+  console.log("Sum's result is : " + data);
+}
+// You are only allowed to call one function after this
+// How will you displayResult of a sum
+const ans = sum(1, 2, displayResult);
 
 
