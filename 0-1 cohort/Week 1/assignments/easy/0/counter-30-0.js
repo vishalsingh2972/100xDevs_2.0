7630b5e0cbbc4b73f2d300c678aedcbba7b4132d
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-04 20:13:41 +0530

    class assignments done

--- 0-1 cohort/Week 1/assignments/easy/0/counter-30-0.js
+++ 0-1 cohort/Week 1/assignments/easy/0/counter-30-0.js
@@ -1,4 +1,5 @@
 //Create a counter in JavaScript(counts down from 30 to 0)
+
 for(let start = 30; start > 0; start--){
   console.log(start);
 }
--- 0-1 cohort/Week 1/assignments/easy/0/terminal-clock.js
+++ 0-1 cohort/Week 1/assignments/easy/0/terminal-clock.js
@@ -1,1 +1,22 @@
 //Create a terminal clock (HH:MM:SS)
+
+//Way 1
+setInterval(() => { //setInterval() for this function to load again and again at every 1000 milliseconds/1 sec
+  let time = new Date().toLocaleTimeString(); //this is function to print time in HH:MM:SS format
+  console.log(time);
+},1000);
+
+
+//Way 2
+function clock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  console.log(`${hours}:${minutes}:${seconds}`);
+}
+setInterval(clock, 1000);
+// To run the function every second we can utilise the setInterval() method.
+// the setInterval() method takes two arguments, a function and a time in milliseconds.
+// It runs the function every time the time specified in milliseconds has passed.
+
