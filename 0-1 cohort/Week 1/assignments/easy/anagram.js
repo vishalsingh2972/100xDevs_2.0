7e9c39759c0440652478e93f82fe57fd71cabaff
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-05 15:46:07 +0530

    Update anagram.js

--- 0-1 cohort/Week 1/assignments/easy/anagram.js
+++ 0-1 cohort/Week 1/assignments/easy/anagram.js
@@ -15,6 +15,28 @@ function isAnagram(str1, str2) {
   str1 = str1.toLowerCase();
   str2 = str2.toLowerCase();
 
+   // split the strings, sort them and store them in form of arr1 and arr2
+  let arr1 = str1.split("").sort()
+  let arr2 = str2.split("").sort();
+
+  //console.log(arr1);
+  //console.log(arr2);
+
+  // function checks if both arrays have same elements at each index position or not
+  function check(arr1, arr2){
+    for(let i=0; i<arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let ans = check(arr1, arr2);
+      return ans;
 }
+
+console.log(isAnagram("ram", "mar"));
+console.log(isAnagram("ram", "mau"));
 
 module.exports = isAnagram;
