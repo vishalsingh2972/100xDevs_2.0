567daf2fc7a3cd022b82717bffaeabd52a3e974d
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-07 13:14:13 +0530

    Objects example added

--- 0-1 cohort/Week 1/assignments/medium/countVowels.js
+++ 0-1 cohort/Week 1/assignments/medium/countVowels.js
@@ -5,36 +5,9 @@
   Once you've implemented the logic, test your code by running
 */
 
-// function countVowels(str) {
-//     // Your code here
-// }
+function countVowels(str) {
+    // Your code here
+}
 
-// module.exports = countVowels;
+module.exports = countVowels;
 
-const allUsers2 = [{ 
-  firstName: "Vishal",
-  gender: "male",
-  metadata:{
-    age: "1",
-    role: "bhakti"
-}
-},{
-  firstName: "Krishna",
-  gender: "male",
-  metadata:{
-      age: "10",
-      role: "Shaktimaan"
-  }
-},{
-  firstName: "Radha",
-  gender: "female",
-  metadata:{
-    age: "12",
-    role: "Shakti"
-}
-}]
-for(let i = 0; i<allUsers2.length; i++){
-  if(allUsers2[i]["metadata"]["role"]=="Shaktimaan"){
-    console.log(allUsers2[i]["firstName"]);
-  }
-}
