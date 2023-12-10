2c6ffe812cadaea37fdfbedb78e63795c8940d08
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-10 21:14:28 +0530

    set

--- 0-1 cohort/Week 1/offline-class-1/level-1/01-String.js
+++ 0-1 cohort/Week 1/offline-class-1/level-1/01-String.js
@@ -16,8 +16,13 @@ getLength("Hello World");
 function findIndexOf(str, target) {
   console.log("Original String:", str);
   console.log("Index:", str.indexOf(target));
+  //console.log("Index:", str.lastIndexOf(target));
 }
-findIndexOf("Hello World", "World");
+findIndexOf("Hello World", "World"); //World starts appearing from the 6th index onwards
+findIndexOf("Vishal singh", "s");
+findIndexOf("Vishal singh", "z");
+findIndexOf("Hello World World", "World");// we get index of first World using .indexOf
+findIndexOf("Hello World World", "World");// we get index of last World using .lastIndexOf
 
 // lastIndexOf
 function findLastIndexOf(str, target) {
@@ -31,21 +36,29 @@ function getSlice(str, start, end) {
   console.log("Original String:", str);
   console.log("After slice:", str.slice(start, end));
 }
-getSlice("Hello World", 0, 5);
+getSlice("Hello World", 0, 5);// starting from 0 until index 5 (does not include index 5 in this, basically [0,5) type)
+console.log("Deepika Padukone".slice(0,9)); //direct attack without writing separate function
 
 // substring
 function getSubstring(str, start, end) {
   console.log("Original String:", str);
   console.log("After substring:", str.substring(start, end));
+  console.log("After substr:", str.substr(start, end));
 }
 getSubstring("Hello World", 0, 5);
+console.log("Deepika Padukone".substr(2,9));//starting from index 2 and length of the substr must be 9
+console.log("Deepika Padukone".slice(2,9)); 
+console.log("Deepika Padukone".substring(2,9));
 
 // replace
 function replaceString(str, target, replacement) {
   console.log("Original String:", str);
   console.log("After replace:", str.replace(target, replacement));
 }
-replaceString("Hello World", "World", "JavaScript");
+replaceString("Republic of India", "India", "Bharat");
+const str = "Hello World";
+console.log(str.replace('world','Mumbo Jumbo'));
+console.log(str.replace('World','JavaScript'));
 
 // split
 function splitString(str, separator) {
@@ -53,6 +66,7 @@ function splitString(str, separator) {
   console.log("After split:", str.split(separator));
 }
 splitString("Hello World", " ");
+splitString("Hello World", "");
 
 // trim
 function trimString(str) {
---
+++ 0-1 cohort/Week 1/offline-class-1/level-1/trial.js
@@ -0,0 +1,5 @@
+function splitString(str, separator) {
+  console.log("Original String:", str);
+  console.log("After split:", str.split(separator));
+}
+splitString("Hello World", "");
