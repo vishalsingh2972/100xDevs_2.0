89f5b039d93889b70cbf0f640d087cbd38a31429
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-12-10 20:58:15 +0530

    Update countVowels.js

--- 0-1 cohort/Week 1/assignments/medium/countVowels.js
+++ 0-1 cohort/Week 1/assignments/medium/countVowels.js
@@ -12,7 +12,7 @@ function countVowels(str) {
   //console.log(str.toLowerCase())
   //console.log(str.toLowerCase().split(""))
 
-  str.toLowerCase().split("").forEach((i) => { //forEach can only iterate accross an array so using to convert string str into an array
+  str.toLowerCase().split("").forEach((i) => { //forEach can only iterate across an array so using to convert string str into an array
     if(vowels.includes(i)){
       count++;
     }
