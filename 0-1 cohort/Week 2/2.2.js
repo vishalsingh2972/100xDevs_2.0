c3657e8e5f125e394c10d4751ef49c90b87f3573
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-01-04 14:59:42 +0530

    2.2

--- 0-1 cohort/Week 2/2.2.js
+++ 0-1 cohort/Week 2/2.2.js
@@ -1,1 +1,7 @@
 //2.2 Node.js runtime, Backend Communication Protocols, Express basics, routes and status codes
+
+// What is ECMAScript?
+// What is JavaScript?
+// What is Node.js?
+// What is Bun?
+
