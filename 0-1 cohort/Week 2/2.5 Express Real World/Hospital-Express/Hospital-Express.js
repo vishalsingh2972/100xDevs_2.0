bf6403344b8fc62dd5e107a75954d0cc28013fd3
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-02-05 23:41:42 +0530

    HospitalExpress.js

--- 0-1 cohort/Week 2/2.5 Express Real World/Hospital-Express/Hospital-Express.js
+++ 0-1 cohort/Week 2/2.5 Express Real World/Hospital-Express/Hospital-Express.js
@@ -1,13 +1,20 @@
 //Express and HTTP with real world examples
 
 // Doctor Example
-//In this lecture, we are building a hospital management system or an in memory hospital using Express.js. 
+//In this lecture, we are building a hospital management system or an in memory hospital(i.e without using a database) using Express.js. 
 // We implemented GET, POST, PUT, and DELETE operations for handling patient information. Additionally, we used Postman to test these endpoints.
 // We created 4 routes (4 things that the hospital can do)
-// 1. GET - User can check how many kidneys they have and their health
-// 2. POST - User can add a new kidney
-// 3. PUT - User can replace a kidney, make it healthy
-// 4. DELETE - User can remove a kidney
+// 1. GET - Going for a consultation to get check up, User can check how many kidneys they have and their health
+// 2. POST - Going to get a new kidney inserted, User can add a new kidney
+// 3. PUT - Going to get a kidney replaced, User can replace a kidney, make it healthy
+// 4. DELETE - Going to get a kidney removed, User can remove a kidney
+
+// Status codes (using Doctor Logic)
+// 1. 200 - Everything went fine
+// 2. 404 - Doctor is not in the hospital
+// 3. 500 - Mid surgery light went away
+// 4. 411 - Inputs were incorrect, wrong person came to surgery
+// 5. 403 - You were not allowed or denied access to the hospital
 
 const express = require("express");
 const app = express();
