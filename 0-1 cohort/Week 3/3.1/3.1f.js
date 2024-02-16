c059fec753c9a109914d09a16c16ea7b62fd394a
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-02-16 23:14:46 +0530

    Authentication

--- 0-1 cohort/Week 3/3.1/3.1f.js
+++ 0-1 cohort/Week 3/3.1/3.1f.js
@@ -9,3 +9,9 @@
 // 1. Give the user back a token on signup/signin 
 // 2. Ask the user to send back the token in all future requests 
 // 3. When the user logs out, ask the user to forget the token (or revoke it from the backend)
+
+// Library to get comfortable with - [jsonwebtokens](https://jwt.io)
+
+// JSON Web Tokens(JWT) is a library:
+// - Commonly used for creating tokens that can be **securely transmitted between parties**.
+// - These tokens contain claims that **can be verified and trusted**, making them a **robust choice for implementing authentication** mechanisms in web applications.
