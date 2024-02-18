c963ced1fb52fa9f07c409acc8b90883cdca14af
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-02-19 03:14:12 +0530

    JWT

--- 0-1 cohort/Week 3/3.2/3.2a.js
+++ 0-1 cohort/Week 3/3.2/3.2a.js
@@ -6,3 +6,25 @@
   //--Encryption
   //--JSON Wen Tokens
   //--Local Storage
+
+//Assignment 1
+//A website which has 2 endpoints:
+
+//endpoint 1
+/*
+POST /signin 
+Body - { 
+username: string 
+password: string 
+} 
+Returns a json web token with username encrypted
+*/
+
+//endpoint 2
+/*
+GET /users 
+Headers - 
+Authorization header 
+Returns an array of all users if user is signed in (token is correct) 
+Returns 403 status code if not
+*/
