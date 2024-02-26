7fadc3fcdaaa28ffa6e8cf75c3b92da8b4c38a49
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-02-26 10:38:59 +0530

    JWT

--- 0-1 cohort/Week 3/3.2/JWTassignment1.js
+++ 0-1 cohort/Week 3/3.2/JWTassignment1.js
@@ -2,7 +2,7 @@
 //A website which has 2 endpoints:
 
 //endpoint 1 - POST /signin which takes username and password as input from FE and Returns a json web token (JWT) with username encrypted
-//endpoint 2 - GET /users which takes Authorization header or previosly returned JWT as input from FE and returns an array of all users if user is signed in (token is correct) or returns 403 status code if not
+//endpoint 2 - GET /users which takes Authorization header or previosly returned JWT as input from FE and Returns an array of all users if user is signed in (token is correct) or Returns 403 status code if not
 
 const express = require("express");
 const jwt = require("jsonwebtoken");
