9c18e31461875205b163c846d91fc0fdda2d8005
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-02-29 00:14:45 +0530

    Mongoose

--- 0-1 cohort/Week 3/3.2/JWTassignment_3.2b.js
+++ 0-1 cohort/Week 3/3.2/JWTassignment_3.2b.js
@@ -1,5 +1,11 @@
 //Assignment 2
-//Same as Assignment 1 but here we have also added Mongoose integration i.e connected the backend server to the MongoDB database via mongoose library
+//Similar as Assignment 1 but here instead of storing data in memory we have added Mongoose integration i.e connected the backend server to the MongoDB database via the mongoose library
+
+//Enpoints
+// 1)/signup - put new user details(username,password,firstname) into the database provided only if same username doesn't already exist in the DB 
+// 2)/signin - backend needs to check if the user already exists in the DB and is their password correct - only if all pass we return the user back with a JWT
+// 3)/users -  we input the header(containing the JWT), backend checks the JWT if all correct it hits the DB gets the user data and sends it back to the FE
+// so in short here we will learn how a backend application talks/interacts with a MongoDB database +  we are using Mongoose library here for interacts between the backend and the MongoDB database
 
 const express = require("express");
 const jwt = require("jsonwebtoken");
