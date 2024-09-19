fb208388ea5e5b2827eb575fb59bbc98632422cc
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-19 20:01:39 +0530

    Update 02-ratelimitter.js

--- 0-1 cohort/Week 3/assignments/01-middlewares/02-ratelimitter.js
+++ 0-1 cohort/Week 3/assignments/01-middlewares/02-ratelimitter.js
@@ -1,7 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const express = require("express");
 const app = express();
+const port = 3001;
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
@@ -12,16 +11,43 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+setInterval(() => { //rest 'numberOfRequestsForUser' as empty after every 1 sec
+  //console.log(numberOfRequestsForUser);
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use(function (req, res, next) {
+  const userId = req.headers["user-id"]; // Get user ID from headers
+
+  // if (!userId) {
+  //   return res.status(400).json({ message: "User ID is required" });
+  // }
+
+  if (numberOfRequestsForUser[userId]) { //requests by the user already exist
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+    //console.log(numberOfRequestsForUser[userId]);
+    if (numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("Rate Limit exceeded bruh");
+    } else {
+      next();
+    }
+  }
+  else{ //first user request so initialise it
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
