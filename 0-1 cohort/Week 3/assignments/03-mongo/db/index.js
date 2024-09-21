492a9694a3dcc149deab5d342d794cfe8a1586f6
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-21 12:18:10 +0530

    middlewares done

--- 0-1 cohort/Week 3/assignments/03-mongo/db/index.js
+++ 0-1 cohort/Week 3/assignments/03-mongo/db/index.js
@@ -1,9 +1,10 @@
+// Import Mongoose Library
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
+// Connect to MongoDB Database
 mongoose.connect('your-mongodb-url');
 
-// Define schemas
+// Define Mongoose Schemas
 const AdminSchema = new mongoose.Schema({
     username: String,
     password: String
@@ -25,10 +26,13 @@ const CourseSchema = new mongoose.Schema({
     imageLink: String
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+// Create Models/Tables/Collections in MongoDB database that will follow the Mongoose Schemas mentioned above
+const Admin = mongoose.model('Admin1', AdminSchema); //Model Instance Creation: you use Admin to create new admin objects/instances, e.g., Admin.create(), new Admin(). //Collection Name in MongoDB: The corresponding collection/table name in your MongoDB database here will be 'Admin1', not Admin.
+const User = mongoose.model('User1', UserSchema);
+const Course = mongoose.model('Course1', CourseSchema);
 
+
+// Export Mongoose Models for Use in Other Files
 module.exports = {
     Admin,
     User,
--- 0-1 cohort/Week 3/assignments/03-mongo/middleware/admin.js
+++ 0-1 cohort/Week 3/assignments/03-mongo/middleware/admin.js
@@ -1,7 +1,26 @@
+//interact with list of db instances present in the 'Admin1' table/collection using the Admin i.e the one which we initially used to create these instances in the table using Admin.create()...
+import { Admin } from "../db/index";
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    Admin.findOne({
+        username: username,
+        password: password
+    }).then(function(value){
+        if (value){
+            next();
+        }
+        else{
+            res.status(403).json({
+                msg: "Admin not found in db, please re-check username/password"
+            })
+        }
+    })
 }
 
 module.exports = adminMiddleware;
