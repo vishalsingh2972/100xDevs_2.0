e56eb0cf1468bf3c02178d5b190943b4321022ec
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-23 19:18:41 +0530

    Generics, enums, imports, exports start

--- 0-1 cohort/Week 9/9.2/node-app/src/a.ts
+++ 0-1 cohort/Week 9/9.2/node-app/src/a.ts
@@ -207,3 +207,15 @@ console.log(filteredUsers([{
     lastName: "Singh",
     age: 16
 }, ]));
+
+
+/*
+Differences Between Interface and Type:
+Interfaces are more suited for defining the structure of an object and can be extended or merged.
+Types are more flexible and can represent more complex scenarios beyond just object structures.
+Types cannot be re-opened to add new properties, while interfaces can be extended.
+
+When to Use Each:
+Use interfaces when you need to define the shape of an object and expect that shape to be extended or used in various parts of your application.
+Use types when you need more flexibility or when you're dealing with unions, intersections, or other advanced type manipulations.
+*/
