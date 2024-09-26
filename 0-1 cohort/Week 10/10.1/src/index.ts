a62404a8141bb68ab11293764a8b60b3864eb4c6
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-26 17:44:45 +0530

    week 10 postgres

---
+++ 0-1 cohort/Week 10/10.1/src/index.ts
@@ -0,0 +1,25 @@
+import { Client } from 'pg';
+
+// Async function to insert data into a table
+async function insertData() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'mysecretpassword',
+  });
+
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion success:', res); // Output insertion result
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
+}
+
+insertData();
