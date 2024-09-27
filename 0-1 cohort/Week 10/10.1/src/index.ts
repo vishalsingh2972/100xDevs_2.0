86f5cd4c74056c3f6290bb564e845a60bf49f107
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-09-27 12:04:52 +0530

    neon postgres working

--- 0-1 cohort/Week 10/10.1/src/index.ts
+++ 0-1 cohort/Week 10/10.1/src/index.ts
@@ -7,20 +7,19 @@ import { postgres_connection_string } from './utils/constants';
 const client = new Client({
   connectionString: postgres_connection_string
 })
-client.connect();
 
 //Querying
 async function createUsersTable() {
-  await client.connect()
+  await client.connect();
   const result = await client.query(`
-      CREATE TABLE users (
-          id SERIAL PRIMARY KEY,
-          username VARCHAR(50) UNIQUE NOT NULL,
-          email VARCHAR(255) UNIQUE NOT NULL,
-          password VARCHAR(255) NOT NULL,
-          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-      );
-  `)
-  console.log(result)
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+  console.log(result);
 }
 createUsersTable();
