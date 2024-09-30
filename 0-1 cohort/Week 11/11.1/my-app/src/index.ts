f024caa5de0e8853cb47efcd25e6bfef04173c1e
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-01 01:44:14 +0530

    hono library for cloudfare workers

--- 0-1 cohort/Week 11/11.1/my-app/src/index.ts
+++ 0-1 cohort/Week 11/11.1/my-app/src/index.ts
@@ -11,8 +11,33 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+//deploy the worker ~ npx wrangler login
+//account details ~ npx wrangler whoami
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// return new Response('Hello World bittu!');
+		return Response.json({
+			message: "hey bittu, this has been deployed"
+		});
 	},
 } satisfies ExportedHandler<Env>;
+
+//routing in Cloudflare environment (the ugly way ~ without any libraries/frameworks)
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		console.log(request);
+// 		// console.log(request.body);
+// 		// console.log(request.headers);
+
+// 		if (request.method === "GET") {
+// 			return Response.json({
+// 				message: "you sent a get request"
+// 			});
+// 		} else {
+// 			return Response.json({
+// 				message: "you did not send a get request"
+// 			});
+// 		}
+// 	},
+// };
