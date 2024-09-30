ea19e6b8a74da80116bc4926fab192f97da64305
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-01 02:46:28 +0530

    hono start working

--- 0-1 cohort/Week 11/11.1/hono-app/src/index.ts
+++ 0-1 cohort/Week 11/11.1/hono-app/src/index.ts
@@ -2,13 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  console.log(c);
-  return c.text('Hello Hono get!')
-})
+// app.get('/', (c) => {
+//   console.log(c); console.log(' ');
+//   console.log(c.req); console.log(' ');
+//   console.log(c.res);
+//   return c.text('Hello Hono get!')
+// })
+// app.post('/user', (c) => {
+//   return c.text('Hello Hono post!')
+// })
 
-app.post('/user', (c) => {
-  return c.text('Hello Hono post!')
+//Getting inputs from user
+app.get('/', async (c) => {
+  const body = await c.req.json()
+  console.log('body : ', body);
+  console.log('Authorization header : ', c.req.header("Authorization"));
+  console.log('Query parameter : ', c.req.query("param"));
+
+  return c.text('Hello Hono!')
 })
 
 export default app
