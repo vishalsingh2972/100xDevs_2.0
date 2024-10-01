fa895d4ae1de086d7699c4c9495da14fd8addfeb
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-02 01:43:52 +0530

    Connection pooling in serverless envs start

--- 0-1 cohort/Week 11/11.1/3-middleware/src/index.ts
+++ 0-1 cohort/Week 11/11.1/3-middleware/src/index.ts
@@ -3,24 +3,28 @@
 import { Hono, Next } from 'hono'
 import { Context } from 'hono/jsx';
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(async (c, next) => {
+async function authMiddleware(c: any, next: any) {
+  //c here is ~ context of this request ~ request(req) and response(res) objects together
   if (c.req.header("Authorization")) {
     // Do validation
-    await next()
+    await next();
   } else {
-    return c.text("You dont have acces");
+    return c.text("You dont have access");
   }
-})
+}
+
+app.use(authMiddleware);
 
 app.get('/', async (c) => {
-  const body = await c.req.parseBody()
+  // app.get('/', authMiddleware, async (c) => { //also works
+  const body = await c.req.parseBody();
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
 
-  return c.json({msg: "as"})
+  return c.json({ msg: "aaaye janaab!" });
 })
 
 export default app
