36fb816f451a6535a2dd02f45d9a159034e813bf
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-01 20:24:46 +0530

    hono middlewares

---
+++ 0-1 cohort/Week 11/11.1/middleware/src/index.ts
@@ -0,0 +1,26 @@
+//Creating a simple auth middleware
+
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx';
+
+const app = new Hono()
+
+app.use(async (c, next) => {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+})
+
+app.get('/', async (c) => {
+  const body = await c.req.parseBody()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.json({msg: "as"})
+})
+
+export default app
