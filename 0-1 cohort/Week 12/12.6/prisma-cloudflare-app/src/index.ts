94572c8145540379566e80af5848e3b991b2c524
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-03 00:58:01 +0530

    error solved 12.6 done

--- 0-1 cohort/Week 12/12.6/prisma-cloudflare-app/src/index.ts
+++ 0-1 cohort/Week 12/12.6/prisma-cloudflare-app/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 //Connect to Prisma Accelerate pool
