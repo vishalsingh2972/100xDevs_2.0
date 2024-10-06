9d2ad572e52f44d2cfc6166702f10b6ca4f39ae1
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-06 20:06:30 +0530

    backend start

--- 0-1 cohort/Week 13/Medium/backend/src/index.ts
+++ 0-1 cohort/Week 13/Medium/backend/src/index.ts
@@ -2,8 +2,34 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+//user signup
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('signup route')
+})
+
+//user signin
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('signin route')
+})
+
+//create a blog
+app.post('/api/v1/blog', (c) => {
+  return c.text('create blog route')
+})
+
+//update a blog
+app.put('/api/v1/blog', (c) => {
+  return c.text('update blog route')
+})
+
+//get a blog given a specific id
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('get a blog route')
+})
+
+//get list of ALL the blogs
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('get all blogs route')
 })
 
 export default app
