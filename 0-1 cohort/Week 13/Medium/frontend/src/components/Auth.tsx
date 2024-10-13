909dbb0761467074ff3f3dd3b37bf2c9e798c371
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-10-14 03:03:11 +0530

    week 14 netxjs

--- 0-1 cohort/Week 13/Medium/frontend/src/components/Auth.tsx
+++ 0-1 cohort/Week 13/Medium/frontend/src/components/Auth.tsx
@@ -9,9 +9,22 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
         </div>
         <div className="text-slate-400 ">
           Already have an account?
-          <Link className="pl-2" to={"/signin"}>Login</Link>
+          <Link className="pl-2 underline" to={"/signin"}>Login</Link>
         </div>
       </div>
     </div>
+  </div>
+}
+
+interface LabelledInputType{
+  label: string,
+  placeholder: string,
+  onchange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function LabelledInput({ label, placeholder, onchange } : LabelledInputType) {
+  return <div>
+    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+    <input onChange={onchange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
   </div>
 }
