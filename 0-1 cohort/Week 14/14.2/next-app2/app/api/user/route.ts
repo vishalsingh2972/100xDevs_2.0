d71ceae4c59b7eb79e553fb6f3f464a6d4f33747
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-11-09 01:15:34 +0530

    db nextjs working

--- 0-1 cohort/Week 14/14.2/next-app2/app/api/user/route.ts
+++ 0-1 cohort/Week 14/14.2/next-app2/app/api/user/route.ts
@@ -1,10 +1,20 @@
 import { NextRequest } from "next/server"
+import { PrismaClient } from "@prisma/client"
+
+const client = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-  
+
   //extract body from incoming req data
   const body = await req.json();
   //console.log(body)
+
+  const user = await client.user.create({
+    data: {
+      username: body.username,
+      password: body.password
+    }
+  });
 
   //store the body data in the database
 
