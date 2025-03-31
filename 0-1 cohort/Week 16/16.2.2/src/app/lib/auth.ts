7b778f1182c6e273204bf103d327482ec3cf39c3
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-01 01:23:25 +0530

    Google and Github provider added

--- 0-1 cohort/Week 16/16.2.2/src/app/lib/auth.ts
+++ 0-1 cohort/Week 16/16.2.2/src/app/lib/auth.ts
@@ -1,4 +1,6 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const NEXT_AUTH = {
   providers: [
@@ -20,6 +22,14 @@ export const NEXT_AUTH = {
           email: "heyminnale@gmail.com"
         };
       }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
