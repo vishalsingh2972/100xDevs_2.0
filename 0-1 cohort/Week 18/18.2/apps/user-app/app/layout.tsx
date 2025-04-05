6dd9ec3eea5d18da38ee7d540bd0623102d7284c
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-06 00:17:46 +0530

    Update layout.tsx

--- 0-1 cohort/Week 18/18.2/apps/user-app/app/layout.tsx
+++ 0-1 cohort/Week 18/18.2/apps/user-app/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Wallet",
-  description: "Simple wallet app",
+  description: "Simple wallet apppppppp",
 };
 
 export default function RootLayout({
