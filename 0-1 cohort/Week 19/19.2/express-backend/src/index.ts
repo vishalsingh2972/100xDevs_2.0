98e5ca71a8532ab705e4194d35cf76c4aae16d52
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-13 01:17:50 +0530

    Redis start

---
+++ 0-1 cohort/Week 19/19.2/express-backend/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+app.use(express.json());
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+app.post("/submit", async (req, res) => {
+    const problemId = req.body.problemId;
+    const code = req.body.code;
+    const language = req.body.language;
+
+    try {
+        await client.lPush("problems", JSON.stringify({ code, language, problemId }));
+        // Store in the database
+        res.status(200).send("Submission received and stored.");
+    } catch (error) {
+        console.error("Redis error:", error);
+        res.status(500).send("Failed to store submission.");
+    }
+});
+
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("Connected to Redis");
+
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startServer();
---
+++ 0-1 cohort/Week 19/19.2/worker/src/index.ts
@@ -0,0 +1,40 @@
+import { createClient } from "redis";
+const client = createClient();
+
+async function processSubmission(submission: string) {
+    const { problemId, code, language } = JSON.parse(submission);
+
+    console.log(`Processing submission for problemId ${problemId}...`);
+    console.log(`Code: ${code}`);
+    console.log(`Language: ${language}`);
+    // Here you would add your actual processing logic
+
+    // Simulate processing delay
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log(`Finished processing submission for problemId ${problemId}.`);
+}
+
+async function startWorker() {
+
+    try {
+        await client.connect();
+        console.log("Worker connected to Redis.");
+
+        // Main loop
+        while (true) {
+            try {
+                const submission = await client.brPop("problems", 0);
+                // @ts-ignore
+                await processSubmission(submission.element);
+            } catch (error) {
+                console.error("Error processing submission:", error);
+                // Implement your error handling logic here. For example, you might want to push
+                // the submission back onto the queue or log the error to a file.
+            }
+        }
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startWorker();
