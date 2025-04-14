a18a912859be206b75f9820793ec050e49806e8b
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-14 23:29:18 +0530

    added pub sub working

---
+++ 0-1 cohort/Week 19/19.2/Redis-Example/pub-sub/src/index.ts
@@ -0,0 +1,22 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+async function startSubscriber() {
+  try {
+    await client.connect();
+    console.log("Subscriber connected to Redis.");
+
+    await client.subscribe("problem_done_pub_sub", (message) => {
+      console.log(`Received message from problem_done_pub_sub: ${message}`);
+      const data = JSON.parse(message);
+      console.log(`Problem ID: ${data.problemId}, Status: ${data.status}`);
+    });
+
+    console.log("Subscriber listening for messages...");
+  } catch (error) {
+    console.error("Failed to connect to Redis", error);
+  }
+}
+
+startSubscriber();
