49f087156a4339dc010edcbd5f505fb1273587d3
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-17 17:07:08 +0530

    OpenAPISpec implemented --- working ✅

--- 1-100 cohort/Week 20/OAS_Trial_App/index.js
+++ 1-100 cohort/Week 20/OAS_Trial_App/index.js
@@ -3,9 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const generated_1 = require("./generated");
 async function main() {
     const response = await generated_1.DefaultService.getUser("44444444");
-    const fetching_way = await fetch("http://127.0.0.1:8787/user/111111");
-    const response2 = await fetching_way.json();
+    //usual way
+    // const fetching_way = await fetch("http://127.0.0.1:8787/user/111111");
+    // const response2 = await fetching_way.json();
     console.log(response);
-    console.log(response2);
+    // console.log(response2);
 }
 main();
