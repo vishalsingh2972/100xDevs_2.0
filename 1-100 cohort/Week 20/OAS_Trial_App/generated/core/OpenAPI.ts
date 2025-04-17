1319ef8fbbe2f6529cb09487353fc87551575eac
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-17 16:26:20 +0530

    try

--- 1-100 cohort/Week 20/OAS_Trial_App/generated/core/OpenAPI.ts
+++ 1-100 cohort/Week 20/OAS_Trial_App/generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://localhost:3000/',
+    BASE: '',
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
