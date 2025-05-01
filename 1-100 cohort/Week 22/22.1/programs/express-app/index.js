f3c723919853e6d23f529d0da23b34a4eb272ea0
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-05-02 01:00:53 +0530

    HS using cluster module

---
+++ 1-100 cohort/Week 22/22.1/programs/express-app/index.js
@@ -0,0 +1,51 @@
+//Implementing Horizontal Scaling within a Single Server in Node.js project using Cluster Module
+//Cluster Module: Horizontal scaling within a single server, utilizing multiple CPU cores to simulate multiple servers.
+//This code example demonstrates horizontal scaling within a single server, utilizing multiple CPU cores to improve performance and responsiveness.
+
+import express from "express";
+import cluster from "cluster";
+import os from "os";
+
+const totalCPUs = os.cpus().length;
+
+const port = 3000;
+
+if (cluster.isPrimary) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  console.log(`Worker ${process.pid} started`);
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("/api/:n", function (req, res) {
+    let n = parseInt(req.params.n);
+    let count = 0;
+
+    if (n > 5000000000) n = 5000000000;
+
+    for (let i = 0; i <= n; i++) {
+      count += i;
+    }
+
+    res.send(`Final count is ${count} ${process.pid}`);
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+}
