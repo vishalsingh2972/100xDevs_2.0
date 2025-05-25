1605c0542e2f96821ada0d9d7fdbbc77d23f3383
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-05-25 23:44:06 +0530

    webrtc brrrrr...

--- 1-100 cohort/Week 23/23.2/webrtc/frontend/src/components/Receiver.tsx
+++ 1-100 cohort/Week 23/23.2/webrtc/frontend/src/components/Receiver.tsx
@@ -10,38 +10,9 @@ export const Receiver = () => {
                 type: 'receiver'
             }));
         }
-        startReceiving(socket);
     }, []);
 
-    function startReceiving(socket: WebSocket) {
-        const video = document.createElement('video');
-        document.body.appendChild(video);
-
-        const pc = new RTCPeerConnection();
-        pc.ontrack = (event) => {
-            video.srcObject = new MediaStream([event.track]);
-            video.play();
-        }
-
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
-            if (message.type === 'createOffer') {
-                pc.setRemoteDescription(message.sdp).then(() => {
-                    pc.createAnswer().then((answer) => {
-                        pc.setLocalDescription(answer);
-                        socket.send(JSON.stringify({
-                            type: 'createAnswer',
-                            sdp: answer
-                        }));
-                    });
-                });
-            } else if (message.type === 'iceCandidate') {
-                pc.addIceCandidate(message.candidate);
-            }
-        }
-    }
-
     return <div>
-
+        Receiver
     </div>
 }
--- 1-100 cohort/Week 23/23.2/webrtc/frontend/src/components/Sender.tsx
+++ 1-100 cohort/Week 23/23.2/webrtc/frontend/src/components/Sender.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react"
 
 export const Sender = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
-    const [pc, setPC] = useState<RTCPeerConnection | null>(null);
 
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8080');
@@ -15,60 +14,17 @@ export const Sender = () => {
         }
     }, []);
 
-    const initiateConn = async () => {
-
-        if (!socket) {
-            alert("Socket not found");
-            return;
-        }
-
-        socket.onmessage = async (event) => {
-            const message = JSON.parse(event.data);
-            if (message.type === 'createAnswer') {
-                await pc.setRemoteDescription(message.sdp);
-            } else if (message.type === 'iceCandidate') {
-                pc.addIceCandidate(message.candidate);
-            }
-        }
-
+    const startSendingVideo = async () => {
         const pc = new RTCPeerConnection();
-        setPC(pc);
-        pc.onicecandidate = (event) => {
-            if (event.candidate) {
-                socket?.send(JSON.stringify({
-                    type: 'iceCandidate',
-                    candidate: event.candidate
-                }));
-            }
-        }
+        const offer = await pc.createOffer(); //SDP
+        await pc.setLocalDescription(offer); //After this line executes, pc.localDescription will now contain the exact same SDP offer data that is stored in the offer variable.
 
-        pc.onnegotiationneeded = async () => {
-            const offer = await pc.createOffer();
-            await pc.setLocalDescription(offer);
-            socket?.send(JSON.stringify({
-                type: 'createOffer',
-                sdp: pc.localDescription
-            }));
-        }
-
-        getCameraStreamAndSend(pc);
-    }
-
-    const getCameraStreamAndSend = (pc: RTCPeerConnection) => {
-        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-            const video = document.createElement('video');
-            video.srcObject = stream;
-            video.play();
-            // this is wrong, should propogate via a component
-            document.body.appendChild(video);
-            stream.getTracks().forEach((track) => {
-                pc?.addTrack(track);
-            });
-        });
+        socket?.send(JSON.stringify({type: 'createOffer', sdp: offer})); //send the offer to the backend signaling server
+        //socket?.send(JSON.stringify({type: 'createOffer', sdp: pc.localDescription})); also works
     }
 
     return <div>
         Sender
-        <button onClick={initiateConn}> Send data </button>
+        <button onClick={startSendingVideo}> Send data </button>
     </div>
 }
