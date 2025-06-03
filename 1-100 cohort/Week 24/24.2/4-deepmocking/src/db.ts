a1ab6ec8a683f3dbe486114d6c9aba868e28536f
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-06-03 21:04:54 +0530

    vitest with deepmocking + adding a db + prisma

--- 1-100 cohort/Week 24/24.2/4-deepmocking/src/db.ts
+++ 1-100 cohort/Week 24/24.2/4-deepmocking/src/db.ts
@@ -3,3 +3,8 @@
 import { PrismaClient } from "./generated/prisma";
 
 export const prismaClient = new PrismaClient();
+
+/* When using deep mocking with vitest-mock-extended, all the properties and methods/functions of the PrismaClient—including nested models operations LITERALLY ALL OF THEM — are automatically mocked and readily available for use during testing.
+console.log(Object.keys(prismaClient)); //list of all properties and methods/functions of the PrismaClient instance now directly available to us
+console.log(Object.keys(prismaClient.sum));
+*/
