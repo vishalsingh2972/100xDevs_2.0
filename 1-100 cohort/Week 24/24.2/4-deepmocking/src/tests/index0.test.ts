10adbffd35c12d6c14a17899ecc310c49f715eda
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-06-04 00:52:25 +0530

    Mocking return values Trials

--- 1-100 cohort/Week 24/24.2/4-deepmocking/src/tests/index0.test.ts
+++ 1-100 cohort/Week 24/24.2/4-deepmocking/src/tests/index0.test.ts
@@ -1,12 +1,11 @@
-//vitest with deepmocking + adding a db + prisma + mockReturnValue
+//vitest with deepmocking + adding a db + prisma
 import { describe, expect, it, vi } from 'vitest';
 import request from "supertest";
 import { app } from '../index';
-import { prismaClient } from '../db';
+//import { userfn } from '../utils/users';
 
-//mockReturnValue
-vi.mock('../db');
-
+vi.mock('../db'); //so now Vitest will not use prismaClient defined in src/db.ts but I am telling it to use the prismaClient defined in src/__mocks__/db.ts instead, vitest-mock-extended finds the src/__mocks__/db.ts file automatically no need to explicitly tell it here.
+//vi.mock('../utils/users');
 
 describe("POST /sum", () => {
   it("should return the sum of two numbers", async () => {
@@ -17,6 +16,8 @@ describe("POST /sum", () => {
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
   });
+
+  //console.log(userfn());
 
   it("should return 411 if no inputs are provided", async () => {
     const res = await request(app).post("/sum").send({});
---
+++ 1-100 cohort/Week 24/24.2/4-deepmocking/src/utils/users/index.ts
@@ -0,0 +1,3 @@
+export const userfn = () => {
+  return "user1";
+}
