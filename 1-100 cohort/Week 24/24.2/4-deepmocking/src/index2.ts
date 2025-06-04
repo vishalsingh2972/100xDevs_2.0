6103fbda4c102ce038bceb16c5a90787c6299e22
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-06-04 20:04:25 +0530

    spys trials

---
+++ 1-100 cohort/Week 24/24.2/4-deepmocking/src/index2.ts
@@ -0,0 +1,56 @@
+//vitest with deepmocking + adding a db + prisma + mockReturnValue + spys
+import express from "express";
+import { z } from "zod";
+import { prismaClient } from "./db";
+
+export const app = express();
+app.use(express.json());
+
+const sumInput = z.object({
+  a: z.number(),
+  b: z.number()
+})
+
+app.post("/sum", async (req, res) => {
+  const parsedResponse = sumInput.safeParse(req.body)
+
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs"
+    })
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  const sum1 = await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer
+    }
+  });
+
+  return res.json({
+    result: sum1.result,
+    id: sum1.id
+  })
+});
+
+app.get("/sum", (req, res) => {
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"])
+  })
+
+  if (!parsedResponse.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs"
+    })
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  return res.json({
+    answer
+  })
+});
---
+++ 1-100 cohort/Week 24/24.2/4-deepmocking/src/tests/index2.test.ts
@@ -0,0 +1,64 @@
+//vitest with deepmocking + adding a db + prisma + mockReturnValue + spys
+import { describe, expect, it, vi } from 'vitest';
+import request from "supertest";
+import { app } from '../index0';
+import { prismaClient } from '../__mocks__/db'; 
+
+vi.mock('../db');
+
+describe("POST /sum", () => {
+  it("should return the sum of two numbers", async () => {
+    //mockReturnValue
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 2,
+      result: 3
+    });
+
+    //spy
+    vi.spyOn(prismaClient.sum, "create");
+
+    const res = await request(app).post("/sum").send({
+      a: 1,
+      b: 2
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+    //expect(res.body.id).toBe(1);
+
+    //spy checks
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3
+      }
+    });
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).post("/sum").send({});
+    expect(res.statusCode).toBe(411);
+    expect(res.body.message).toBe("Incorrect inputs");
+  });
+
+});
+
+describe("GET /sum", () => {
+  it("should return the sum of two numbers", async () => {
+
+    const res = await request(app).get("/sum").set({
+      a: "1",
+      b: "2"
+    }).send();
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).get("/sum").send();
+    expect(res.statusCode).toBe(411);
+    expect(res.body.message).toBe("Incorrect inputs");
+  });
+});
