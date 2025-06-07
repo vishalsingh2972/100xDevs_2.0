b277b8c25e715c7d794e59759b947630d7ab5443
Vishal Singh <vishalsinghpanwar02@gmail.com>
2025-06-08 03:08:37 +0530

    Week 25 E/E testing

--- 1-100 cohort/Week 25/2-end-to-end-test/cypress/e2e/1-getting-started/cms.cy.js
+++ 1-100 cohort/Week 25/2-end-to-end-test/cypress/e2e/1-getting-started/cms.cy.js
@@ -9,7 +9,7 @@ describe('Testing app', () => {
     cy.contains('Login').should('exist'); //Will find the text 'Login' on the page & check if it exists.
     cy.contains('Login').click(); //Next, will click on this Login button
     cy.contains('Signin to your Account').should('exist', { timeout: 10000 }); //will wait for 10 seconds and find the text 'Signin to your Account' on the page & check if it exists.
-    cy.get('#email').type('harkirat.iitr@gmail.com'); //will find the input field with id 'email' and type the given email address.
+    cy.get('#email').type('vishalsingh@gmail.com'); //will find the input field with id 'email' and type the given email address.
 
     // Fill in the password field
     cy.get('#password').type('123random'); //will find the input field with id 'password' and type the given password.
